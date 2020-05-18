5b63aef9750e8aebb99dfd06c7a80b1bcc16698b
marinatangerine <suarezlopezmarina@gmail.com>
2020-05-18 21:43:06 +0200

    changed project structure

---
+++ src/main/mainProgram.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package main;
+
+/**
+ * @author suare
+ *
+ */
+public class mainProgram {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+	}
+
+}
