9fa91d111eff6c84095cd0ee44c84a60ba119711
eramoscar <eramoscar@uoc.edu>
2020-05-22 22:03:31 +0200

    Added package pruebasUnitarias. Opcion: JUnit Jupiter Test

---
+++ src/pruebasUnitarias/ListadoPersonasTest.java
@@ -0,0 +1,14 @@
+package pruebasUnitarias;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ListadoPersonasTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
