f32c93d2bb927d8e5687c7480c0f2f4a0a6383b0
Eric <eramoscar@uoc.edu>
2020-05-23 12:03:03 +0200

    (borrado package pruebasUnitarias y added testsclases en el main) (cambio a libreria a JUnit Test 4) (metodos seleccionados de Persona y Voluntario)

---
+++ src/main/VoluntarioTest.java
@@ -0,0 +1,24 @@
+package main;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class VoluntarioTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void testSetAreaActividades() {
+		fail("Not yet implemented");
+	}
+
+}
